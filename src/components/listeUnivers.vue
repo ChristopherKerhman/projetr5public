7cf6b5536dc319718e6bc37c83418d0992f5e1bf
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2022-08-24 16:31:52 +0200

    Règles spéciales ok !

--- src/components/listeUnivers.vue
+++ src/components/listeUnivers.vue
@@ -16,12 +16,24 @@
     <article v-if="OneFigurine != null">
       <h3>Details de la figurine</h3>
       <ul>
-        <li>Nom figurine : {{OneFigurine.nomFigurine}} - {{OneFigurine[0].prixFigurine}} points</li>
+        <li>Nom figurine : {{OneFigurine.nomFigurine}} - {{role[OneFigurine.role]}} - {{OneFigurine[0].prixFigurine}} points</li>
         <li>Mouvement : {{OneFigurine.mouvement}}" / {{(Math.round((OneFigurine.mouvement)*1.4))}}" + 1D4"</li>
         <li>Présentation : {{OneFigurine.texteFigurine}}</li>
         <li>DQM: {{de[OneFigurine.DQM]}} / DC: {{de[OneFigurine.DC]}}</li>
         <li>Sauvegarde : {{armure[OneFigurine.svg]}} / Point de vie: {{OneFigurine.pdv}}</li>
         <li>Règle spécial de la figurine : <strong v-for="p in RSFigurine" :key="p">{{p.nomRS}}.</strong> </li>
+        <li>Armes:
+          <ul v-for="q in OneFigurine[2]" :key="q">
+            <li v-if="q.typeArme > 0">
+              {{q.nomArme}} - Type {{typeArme[q.typeArme]}} Range :{{q.range}}"/ {{Math.round((q.range)*2.54)}} cm
+              - {{puissance[q.puissance]+de[OneFigurine.DC]}} -
+            </li>
+            <li v-if="q.gabarit >0">Gabarit de l'arme : {{gabarit[q.gabarit]}}</li>
+            <li v-else>{{q.nomArme}} - Type {{typeArme[q.typeArme]}} - {{puissance[q.puissance]+de[OneFigurine.DC]}}</li>
+            <li v-for="t in q[0]" :key="t">Règles spéciales : <strong v-for="u in t.RS" :key="u">{{u}}.</strong></li>
+          </ul>
+        </li>
+
       </ul>
 
     </article>
@@ -41,7 +53,12 @@ export default {
       OneFigurine: null,
       de: ['D6', 'D8', 'D10', 'D12'],
       armure: ['Aucune', '6+', '5+', '4+', '3+', '2+'],
-      RSFigurine: null
+      typeArme: ['Close', 'Tir', 'Explosive'],
+      role: ['Civile', 'Conscrit', 'Soldat professionnel', 'Elite', 'Officier', 'Officier suppérieur'],
+      gabarit: ['Pas de gabarit', 'Petit', 'Moyen', 'Grand', 'Cône'],
+      puissance: [1, 2, 3, 4, 5, 6],
+      RSFigurine: null,
+      RSArme: ['A']
     }
   },
   mounted () {
