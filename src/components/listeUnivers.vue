6d7995441a6d9fea76412dfc3afd7cbeeb4560dc
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2022-08-24 11:50:11 +0200

    Affichage une unité

--- src/components/listeUnivers.vue
+++ src/components/listeUnivers.vue
@@ -1,14 +1,32 @@
 <template>
   <section>
-    <ul>
-      <li v-for="l in listeUnivers" v-bind:key="l" @click="searchFactions(l.idUnivers, l.nomUnivers)">{{l.nomUnivers}}</li>
-    </ul>
-    <ul  v-if="listeFaction != null">
-      <li><h3>{{nomUnivers}}</h3></li>
-      <li v-for="k in listeFaction" v-bind:key="k">{{k.nomFaction}}</li>
-    </ul>
+    <article>
+      <h3>Les univers et faction disponible</h3>
+      <ul>
+        <li v-for="l in listeUnivers" v-bind:key="l" @click="searchFactions(l.idUnivers, l.nomUnivers)">{{l.nomUnivers}}</li>
+      </ul>
+      <ul  v-if="listeFaction != null">
+        <li><h3>{{nomUnivers}}</h3></li>
+        <li v-for="k in listeFaction" v-bind:key="k" @click="listeFigurine(k.idFaction, k.nomFaction)">{{k.nomFaction}}</li>
+      </ul>
+      <ul v-if ="listeFigurines != null">
+          <li v-for="m in listeFigurines" v-bind:key="m" @click="affichage(m)">{{m.nomFigurine}} Prix figurine : {{[m][0][0].prixFigurine}} points</li>
+      </ul>
+    </article>
+    <article v-if="OneFigurine != null">
+      <h3>Details de la figurine</h3>
+      <ul>
+        <li>Nom figurine : {{OneFigurine.nomFigurine}} - {{OneFigurine[0].prixFigurine}} points</li>
+        <li>Mouvement : {{OneFigurine.mouvement}}" / {{(Math.round((OneFigurine.mouvement)*1.4))}}" + 1D4"</li>
+        <li>Présentation : {{OneFigurine.texteFigurine}}</li>
+        <li>DQM: {{de[OneFigurine.DQM]}} / DC: {{de[OneFigurine.DC]}}</li>
+        <li>Sauvegarde : {{armure[OneFigurine.svg]}} / Point de vie: {{OneFigurine.pdv}}</li>
+        <li>Règle spécial de la figurine : <strong v-for="p in RSFigurine" :key="p">{{p.nomRS}}.</strong> </li>
+      </ul>
 
+    </article>
   </section>
+
 </template>
 <script>
 export default {
@@ -17,7 +35,13 @@ export default {
     return {
       listeUnivers: null,
       listeFaction: null,
-      nomUnivers: null
+      nomUnivers: null,
+      nomFaction: null,
+      listeFigurines: null,
+      OneFigurine: null,
+      de: ['D6', 'D8', 'D10', 'D12'],
+      armure: ['Aucune', '6+', '5+', '4+', '3+', '2+'],
+      RSFigurine: null
     }
   },
   mounted () {
@@ -40,6 +64,20 @@ export default {
         })
         .catch(e => {
         })
+    },
+    listeFigurine (idF, nomF) {
+      this.nomFaction = nomF
+      fetch('https://apir5.graines1901.com/sources/APIpublic/listeFigurine.php?idFaction=' + idF)
+        .then(response => response.json())
+        .then(response => {
+          this.listeFigurines = response
+        })
+        .catch(e => {
+        })
+    },
+    affichage (detailsFigurine) {
+      this.OneFigurine = detailsFigurine
+      this.RSFigurine = this.OneFigurine[1]
     }
   }
 }
